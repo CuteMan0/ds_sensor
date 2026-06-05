abba17ba709fe110f738432e379e197ed8ca65b4
wsx <sixian.w@qq.com>
2026-06-05 10:17:41 +0800

    change

--- Hardware/DS136氧化还原传感器/Output documents/Program Project/DS136/App/key_handler.h
+++
@@ -1,7 +0,0 @@
-#ifndef _KEY_HANDLER_H_
-#define _KEY_HANDLER_H_
-
-void key_init(void);
-void key_scan(void);
-
-#endif
--- Hardware/DS136氧化还原传感器/Output documents/Program Project/DS136/Driver/USB_CDC_drive.h
+++
@@ -1,6 +0,0 @@
-#ifndef	__USB_CDC_drive_H
-#define	__USB_CDC_drive_H
-
-void USB_CDC_Initialization(void);
-
-#endif
